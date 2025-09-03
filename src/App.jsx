e73ce0c8c1965f78b575bbdf66d2290c9cbe0cc4
ilkayalparslan <ilkayalparslan@gmail.com>
2025-09-03 20:08:48 +0300

    Initial commit: Complete EbruLoria real estate website with React, Tailwind CSS, and hotel listings

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation.jsx'
+import Home from './pages/Home.jsx'
+import About from './components/About.jsx'
+import FloatingButtons from './components/FloatingButtons.jsx'
+import Footer from './components/Footer.jsx'
+import Hotels from './pages/Hotels.jsx'
+import './index.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<><Navigation /><Home /><Footer /><FloatingButtons /></>} />
+        <Route path='/about' element={<><Navigation /><About /><Footer /><FloatingButtons /></>} />
+        <Route path='/hotels' element={<Hotels />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
